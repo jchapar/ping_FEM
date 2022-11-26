2a10345abc075e2beb1d099f445fb6d6b592ee73
josechaparro <josechaparro.dev@gmail.com>
2022-11-26 13:14:40 -0700

    styled classes added for mobile screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        blue: 'hsl(223, 87%, 63%)',
         paleBlue: 'hsl(223, 100%, 88%)',
         lightRed: 'hsl(354, 100%, 66%)',
         gray: 'hsl(0, 0%, 59%)',
