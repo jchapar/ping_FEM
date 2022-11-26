518cae71cc1bc69ead2ceec30b4b853326a0ea7c
josechaparro <josechaparro.dev@gmail.com>
2022-11-25 23:58:23 -0700

    all markup added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./*.html'],
+  screens: {
+    sm: '480px',
+    md: '768px',
+    lg: '976px',
+    xl: '1440px',
+  },
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        paleBlue: 'hsl(223, 100%, 88%)',
+        lightRed: 'hsl(354, 100%, 66%)',
+        gray: 'hsl(0, 0%, 59%)',
+        veryDarkBlue: 'hsl(209, 33%, 12%)',
+      },
+      fontFamily: {
+        sans: ['Libre Franklin', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
